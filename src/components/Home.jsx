a87d2dfd5564ea200536771236581f7d09d4c4d5
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-23 16:16:09 -0700

    Working on landing page

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Beige Cashmere</h1>
+                <p>We’re beige cashmere and we specialize in video production, video editing , and photography</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
