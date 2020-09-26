2c402b61f5d0575dbd168a9c532bade8d40d84ba
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-09-26 15:53:45 -0700

    working on deployment and embedding videos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Video from './VideoEmbed';
 
 class Projects extends Component {
     render() {
         return (
             <div>
                 <h1>PROJECTS</h1>
-
+                <Video />
             </div>
         );
     }
---
+++ src/components/VideoEmbed.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactPlayer from "react-player"
+
+function Video() {
+    return (
+        <div>
+            <ReactPlayer
+                url="https://vimeo.com/461896496"
+            />
+        </div>
+    )
+}
+
+export default Video
