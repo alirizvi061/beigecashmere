f29d3196ca411ce83ac75c8cbf20ef0b0b6ded24
Senthil <spk2dc@virginia.edu>
2020-10-01 02:47:56 -0400

    filling videos into accordion arrays

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,120 +6,73 @@ class Projects extends Component {
   render() {
     let commercialVideos = [
       [
-        {
-          title: "Gravityball golf",
-          src: "https://player.vimeo.com/video/461894477",
-        },
-        {
-          title: "Gravity Ball",
-          src: "https://player.vimeo.com/video/461896496",
-        },
-        {
-          title: "Claire Proft Commercial 2",
-          src: "https://player.vimeo.com/video/460807710",
-        },
+        { title: "AutoMax Exotics Street Commercial", id: "460735401" },
+        { title: "AutoMax Exotics Commercial", id: "460735203" },
+        { title: "Lady D Cosmetics BTS Commercial", id: "460733901" },
       ],
       [
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460807902",
-        },
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460741963",
-        },
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460735401",
-        },
+        { title: "Bala Enzyme Commercial 4", id: "460732074" },
+        { title: "Lady D Lashes Commercial", id: "460731842" },
+        { title: "Bala Enzyme Commercial 3", id: "460730949" },
       ],
       [
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460719413",
-        },
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460733901",
-        },
-        {
-          title: "Bala Enzyme Commercial 4",
-          src: "https://player.vimeo.com/video/460732074",
-        },
+        { title: "Bala Enzyme Commercial 2", id: "460730755" },
+        { title: "Bala Enzyme Commercial 1", id: "460730623" },
+        { title: "Mian Vienna Hair Commercial", id: "460724735" },
       ],
       [
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460730949",
-        },
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460733901",
-        },
-        {
-          title: "Bala Enzyme Commercial 2",
-          src: "https://player.vimeo.com/video/460730755",
-        },
+        { title: "USA Allegiance Commercial 4", id: "460724039" },
+        { title: "USA Allegiance Commercial 3", id: "460723936" },
+        { title: "USA Allegiance Commercial 2", id: "460723696" },
       ],
       [
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460730623",
-        },
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460724735",
-        },
-        {
-          title: "LA Vitality MD Commercial 1",
-          src: "https://player.vimeo.com/video/460718607",
-        },
+        { title: "USA Allegiance Commercial 1", id: "460723519" },
+        { title: "Amelia Grace Commercial", id: "460722095" },
+        { title: "LA Vitality MD Commercial3", id: "460719800" },
       ],
       [
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460723936",
-        },
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460723696",
-        },
-        {
-          title: "USA Allegiance Commercial 1",
-          src: "https://player.vimeo.com/video/460723519",
-        },
+        { title: "LA Vitality MD Commercial2", id: "460719413" },
+        { title: "LA Vitality MD Commercial1", id: "460718607" },
+        { title: "Solar Bank Commercial3", id: "460715965" },
       ],
       [
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460722095",
-        },
-        {
-          title: "",
-          src: "https://player.vimeo.com/video/460720521",
-        },
-        {
-          title: "LA Vitality MD Commercial 3",
-          src: "https://player.vimeo.com/video/460719800",
-        },
+        { title: "Solar Bank Commercial2", id: "460714992" },
+        { title: "Solar Bank Commercial1", id: "460714697" },
+        { title: "Talosys Commercial 1", id: "460707185" },
+      ],
+      [
+        { title: "Talosys Commercial 2", id: "460706925" },
+        { title: "IV Organics Commercial", id: "460702354" },
+        { title: "UO STUDIOS Commercial", id: "460434871" },
       ],
     ];
 
     let musicVideos = [
       [
-        {
-          title: "Bitter Moments - Chris Marr",
-          src: "https://player.vimeo.com/video/460430651",
-        },
-        {
-          title: "Blessings - Gee Kazz",
-          src: "https://player.vimeo.com/video/460430336",
-        },
-        {
-          title: "Gimme That Nana - Steve Medea",
-          src: "https://player.vimeo.com/video/460430197",
-        },
+        { title: "Bitter Moments - Chris Marr", id: "460430651" },
+        { title: "Blessings - Gee Kazz", id: "460430336" },
+        { title: "Gimme That Nana - Steve Medea", id: "460430197" },
       ],
+      [
+        { title: "AMPM - Pe$ovelli", id: "460429619" },
+        { title: "Ride For You - Ladini", id: "460429386" },
+        { title: "I See You - Keany", id: "460429185" },
+      ],
+      [
+        { title: "Heavy In The Streets - FTLCOOLAID", id: "460428904" },
+        { title: "Facts - FTLCOOLAID", id: "460428501" },
+      ],
+      [
+        { title: "What I Want - Coolest Kid AJ", id: "460427859" },
+        { title: "Car/Gaari - Siddzartha", id: "460426964" },
+        { title: "Bust Down Necklace - Nat$uki", id: "460425662" },
+      ],
+      [
+        { title: "Confession - CAM", id: "460426477" },
+        { title: "My Dawg - CAM", id: "460426758" },
+        { title: "Bleed - VK Belle", id: "460425375" },
+      ],
+      [{ title: "California Girl - Ju$to", id: "460425101" }],
     ];
 
     function accordionClick(event) {
@@ -149,9 +102,9 @@ class Projects extends Component {
                 {row.map((video) => {
                   return (
                     <iframe
-                      key={`key_${video.src}`}
+                      key={`key_${video.id}`}
                       title={video.title}
-                      src={video.src}
+                      src={`https://player.vimeo.com/video/${video.id}`}
                       width='640'
                       height='160'
                       frameBorder='0'
@@ -179,9 +132,9 @@ class Projects extends Component {
                 {row.map((video) => {
                   return (
                     <iframe
-                      key={`key_${video.src}`}
+                      key={`key_${video.id}`}
                       title={video.title}
-                      src={video.src}
+                      src={`https://player.vimeo.com/video/${video.id}`}
                       width='640'
                       height='160'
                       frameBorder='0'
