2e7ec8a35ab1d82531902e61ed35c03403560fa7
Senthil <spk2dc@virginia.edu>
2020-10-01 02:53:56 -0400

    fixing projects nav link to go directly to project page

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
-import '../App.css';
+import React, { Component } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 class NavMenu extends Component {
   render() {
     return (
       <div>
         <Navbar
-          data-spy="scroll"
-          data-target=".navbar"
-          data-offset="50"
-          className="nav fixed-top"
+          data-spy='scroll'
+          data-target='.navbar'
+          data-offset='50'
+          className='nav fixed-top'
           // bg="dark"
-          expand="lg"
+          expand='lg'
         >
           {/* <Navbar.Brand href="/">Beige Cashmere</Navbar.Brand> */}
-          <Link to="/">
-            <img src="logoOnly.png" alt="" className="navLogo" />
+          <Link to='/'>
+            <img src='logoOnly.png' alt='' className='navLogo' />
           </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link className="navButtons" href="/">HOME</Nav.Link>
-              <Nav.Link href="#projectsScroll">PROJECTS</Nav.Link>
-              <Nav.Link href="#locationsScroll">LOCATIONS</Nav.Link>
-              <Nav.Link href="#formScroll">BOOK NOW</Nav.Link>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link className='navButtons' href='/'>
+                HOME
+              </Nav.Link>
+              <Nav.Link href='/projects'>PROJECTS</Nav.Link>
+              <Nav.Link href='#locationsScroll'>LOCATIONS</Nav.Link>
+              <Nav.Link href='#formScroll'>BOOK NOW</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
