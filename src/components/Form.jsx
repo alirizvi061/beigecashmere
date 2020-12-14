547345b5b6a16ce31182a47fe85060d7e401a061
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-12-13 19:40:55 -0800

    Form working with EmailJS and Footer copyright label updated to 2021

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, {useState}from "react";
 import emailjs from 'emailjs-com'
 
 export default function Form() {
 
-  function sendEmail(e) {
-    e.preventDefault();
 
-    emailjs.sendForm('service_79x9b9l', 'template_n14srsh', e.target, 'user_YIptbOHIy9sETu4BiabOb')
+
+  const [messageSent, setMessageSent] = useState(false)
+  const [formState, setFormState] = useState(false)
+
+  function sendEmail(e) {
+    console.log(e.target, "new new")
+    e.preventDefault();
+    if(e.target === null){
+      setFormState(true)
+    } else {
+    emailjs.sendForm('service_13bnb3e', 'template_kh5fogk', e.target, 'user_9EF7CbKJH32e0KnAqQOLa')
       .then((result) => {
+        setMessageSent(true)
         console.log(result.text);
       }, (error) => {
         console.log(error.text);
       });
     e.target.reset()
+    }
+    
   }
 
+  if(messageSent){
+    setTimeout(() => {
+      setMessageSent(false)
+    }, [3000])
+  }
 
   return (
     <div id="formScroll" className='form'>
       <h2>REQUEST A FREE QUOTE</h2>
-      <form
+
+      {/* <form
         action='mailto:info@beigecashmere.com'
         method='post'
         name='EmailForm'
@@ -46,27 +63,30 @@ export default function Form() {
         <br />
         <input type='submit' value='EMAIL' />
         <br />
-      </form>
+      </form> */}
 
-      {/* <form className="contact-form" onSubmit={sendEmail}>
+      <form className="contact-form" onSubmit={sendEmail}>
         <input type="text" placeholder='NAME' name='Name' />
-        <label>Name</label>
+
         <br />
 
         <input type='text' placeholder='EMAIL' name='Email' />
-        <label>Email</label>
+
         <br />
 
         <input type='text' placeholder='PHONE' name='Phone' />
-        <label>Phone</label>
+
         <br />
 
         <textarea type='text'
           placeholder='TELL US ABOUT YOUR PROJECT'
           name='Message' ></textarea>
-        <label>Message</label>
+
         <input type="submit" value="Send" />
-      </form> */}
+      </form>
+      {messageSent && <p>Your Message Has Been Sent!</p>}
+      {formState && <p>Please Complete All Fields Before Submitting!</p>}
+
     </div>
   );
 
